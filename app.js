4599561d292b1f1b0a7286217f3e1ff83d221ed7
Arthur <arttriantos@gmail.com>
2021-05-27 14:57:19 -0500

    Made progress on part 2

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.body.style.backgroundColor = "blanchedalmond";
+let div = document.createElement('div');
+let para = document.createElement('p');
+let h3 = document.createElement('h3');
+let button = document.createElement('button');
+document.body.appendChild(button);
 let myFriends = ["John", "Xander", "Jack", "Justin", "Skyler"];
+button.innerText = 'Sing!';
+button.addEventListener ('click', function () {
+    codeSong();
+});
+
+
+
 
 function codeSong() {
     for (let j = 0; j < myFriends.length; j++) {
         var lines;
         var linesLeft;
+        let div = document.createElement('div');
+        let h3 = document.createElement('h3');
+        h3.innerText = myFriends[j];
         for (i=99; i >= 1; i--) {
             if (i == 1) {
                 lines = "line";
@@ -21,4 +38,6 @@ function codeSong() {
     
 }
 
-codeSong();
+
+
+});
